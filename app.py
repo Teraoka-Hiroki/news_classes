9f555ac6c39e0d75fb328a79866a41e7e557d720
Teraoka-Hiroki <88216511+Teraoka-Hiroki@users.noreply.github.com>
2023-09-10 10:29:23 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ result_path = combine_multiple_paths(path1, path2)
 
 
 url2=['.', 'https://drive.google.com/file/d/1-1ZDrx8LvR4wdD654sBdua2il7l20Q-n/view?usp=sharing']
-
-loaded_model = BertForSequenceClassification.from_pretrained(url2)
+url3=['.']
+loaded_model = BertForSequenceClassification.from_pretrained(url3)
 loaded_model.cuda() 
-loaded_tokenizer = BertJapaneseTokenizer.from_pretrained(url2)
+loaded_tokenizer = BertJapaneseTokenizer.from_pretrained(url3)
 
 st.title("「ニュースの分類」アプリ")
 st.write("###### モデル ：Pretrained, Japanese BERT models （東北大学　乾研究室）")
